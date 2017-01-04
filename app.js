e77934aa35e4acb36d9054b5d835b86bd010f1ba
aisflat439 <fitzsimonsdevin@gmail.com>
2017-01-04 09:47:00 -0500

    add procfile

--- app.js
+++ app.js
@@ -13,5 +13,14 @@ app.listen(port, function() {
 })
 
 app.post('/hello', function(req, res, next){
-  var userName = req.body.user_name
+  var username = req.body.user_name;
+  var botPayload = {
+    text: "Hello " + username + " This is neat!";
+  };
+
+  if (username !== 'slackbot'){
+    return res.status(200).json(botPayload)
+  } else {
+    return res.status(200).end();
+  }
 })
